b85a01d99e4410b20374b523084960722df22e91
Thomas Breno Bertelli Mendes <bertellimendes@gmail.com>
2023-08-24 22:52:06 -0300

    feat: add pagina nao encontrada

--- roteamento/src/app/app.module.ts
+++ roteamento/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
 import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
 
 
 
@@ -12,7 +13,8 @@ import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.componen
   declarations: [
     AppComponent,
     PrimeiraPaginaComponent,
-    SegundaPaginaComponent
+    SegundaPaginaComponent,
+    PaginaNaoEncontradaComponent
   ],
   imports: [
     BrowserModule,
