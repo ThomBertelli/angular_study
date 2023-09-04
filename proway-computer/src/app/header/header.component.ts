263e0f953df0878d9db21ac39ceec7f040385a6c
Thomas Breno Bertelli Mendes <bertellimendes@gmail.com>
2023-09-04 20:34:31 -0300

    feat(proway-computer): add carrinho service

--- proway-computer/src/app/header/header.component.ts
+++ proway-computer/src/app/header/header.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CarrinhoService } from '../carrinho.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit  {
+
+  qtdItensCarrinho = 0
+
+  constructor(
+    public carrinhoService : CarrinhoService
+  ){ }
+
+  ngOnInit(): void {
+    const qtdItensCarrinho = this.carrinhoService.obtemCarrinho().length
+    if(qtdItensCarrinho > 0){
+      this.qtdItensCarrinho = qtdItensCarrinho
+    }
+  }
+
+  
+
+    
 
 }
